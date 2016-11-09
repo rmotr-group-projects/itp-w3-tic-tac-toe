4cd511abd4901aa78f5e1fb6a1cdf7dbd1a49aa9
Raghu <raghureddy031@gmail.com>
2016-11-09 23:49:56 +0000

    Tests passed

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,25 +1,73 @@
-class Game(object):
+from tic_tac_toe.exceptions import InvalidMovementException
 
+class Game(object):
+    winner = None
+    
     def __init__(self, board, player_1, player_2):
-        pass
+        self.board = board
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.turn_count = 1
 
     def is_finished(self):
-        pass
+        if self.has_winner():
+            return True
+        for x in self.board.board:
+            for y in x:
+                if y is None:
+                    return False
+        else:
+            return True
 
     def has_winner(self):
-        pass
-
+        for x in range(3):
+            l = self.board.get_row(x)
+            if l[0] == l[1] == l[2]:
+                if l[0] is not None:
+                    self.winner = l[0]
+                    return True
+            l = self.board.get_column(x)
+            if l[0] == l[1] == l[2]:
+                if l[0] is not None:
+                    self.winner = l[0]
+                    return True
+            l = self.board.get_diagonal(x)
+            if l[0] == l[1] == l[2]:
+                if l[0] is not None:
+                    self.winner = l[0]
+                    return True
+        else:
+            return False
+        
     def is_tied(self):
-        pass
+        if self.winner is None and self.is_finished():
+            return True
 
     def get_winner(self):
-        pass
+        return self.winner
 
     def next_turn(self):
-        pass
+        if self.turn_count % 2 == 0:
+            return self.player_2
+        else:
+            return self.player_1
 
     def move(self, player, row, col):
-        pass
+        
+        if (player not in ('X', 'O') or 
+          self.next_turn() is not player or
+           (row > 2) or (row < 0) or 
+           (col > 2) or (col < 0) or 
+           self.is_tied is True or 
+           self.winner is not None or
+           self.is_finished is True):
+               raise InvalidMovementException()
+            
+        if self.board.board[row][col] is None:
+            self.board.board[row][col] = player
+        else:
+            raise InvalidMovementException()
+        self.turn_count += 1
 
 
 BOARD_TEMPLATE = """
@@ -37,19 +85,55 @@ _____|_____|_____
 
 class Board(object):
     def __init__(self, initial_board=None):
-        pass
+        if initial_board is None:
+            self.board = [
+                [None, None, None],[None, None, None],[None, None, None]
+            ]
+        else:
+            self.board = initial_board
 
     def move(self, figure, row, col):
-        pass
-
+        self.board[row][col] = figure
+        
     def __str__(self):
-        return BOARD_TEMPLATE  # .format()
+        board_str = BOARD_TEMPLATE.format(
+        self.board[0][0] or '-',
+        self.board[0][1] or '-',
+        self.board[0][2] or '-',
+	
+        self.board[1][0] or '-',
+        self.board[1][1] or '-',
+        self.board[1][2] or '-',
+	
+        self.board[2][0] or '-',
+        self.board[2][1] or '-',
+        self.board[2][2] or '-',
+)
+        return board_str
 
     def get_row(self, row_number):
-        pass
+        return self.board[row_number]
 
     def get_column(self, col_number):
-        pass
+        column = []
+        list1 = []
+        for i in range(len(self.board)):
+            list1 = self.board[i]
+            column.append(list1[col_number])
+        return column
 
     def get_diagonal(self, row_number):
-        pass
+        diag = []
+        if row_number == 0:
+            diag.append(self.board[0][0])
+            diag.append(self.board[1][1])
+            diag.append(self.board[2][2])
+        elif row_number == 1:
+            diag.append(self.board[0][2])
+            diag.append(self.board[1][1])
+            diag.append(self.board[2][0])
+        else:
+            return [None, None, None]
+        return diag
+        
+            
