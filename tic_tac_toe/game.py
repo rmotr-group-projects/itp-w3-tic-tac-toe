9a9f61155232540afb3356e78e44b99cc6a911b6
Ahmed Eid <a.eid@yandex.com>
2016-11-09 03:37:21 +0000

    Done G5

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,25 +1,67 @@
-class Game(object):
+from exceptions import InvalidMovementException
 
-    def __init__(self, board, player_1, player_2):
-        pass
+class Game(object):
+    def __init__(self, board , player_1="X", player_2="Y"):
+        self.p1 = player_1
+        self.p2 = player_2
+        self.finished = False
+        self.there_is_winner = False
+        self.tied = False
+        self.winner = None
+        self.next = self.p1
+        self.board = board
 
     def is_finished(self):
-        pass
+        if(self.has_winner()):
+            self.finished = True
+        elif ( None not in self.board.get_diagonal(0) or  None not in self.board.get_diagonal(1)):
+            self.finished = True 
+        return self.finished
 
     def has_winner(self):
-        pass
+        for i in range(3):
+            if self.board.get_column(i) == ['X' , 'X' , 'X']:
+                self.winner = 'X'
+            elif self.board.get_column(i) == ['Y' , 'Y' , 'Y']:
+                self.winner = 'Y'
+            if self.board.get_row(i) == ['X' , 'X' , 'X']:
+                self.winner = 'X'
+            elif self.board.get_row(i) == ['Y' , 'Y' , 'Y']:
+                self.winner = 'Y'
+
+        if self.board.get_diagonal(0) == ['X' , 'X' , 'X']:
+            self.winner = 'X'
+        elif self.board.get_diagonal(0) == ['Y' , 'Y' , 'Y']:
+            self.winner = 'Y'
+        if self.board.get_diagonal(1) == ['X' , 'X' , 'X']:
+            self.winner = 'X'
+        elif self.board.get_diagonal(1) == ['Y' , 'Y' , 'Y']:
+            self.winner = 'Y'
+            
+        if self.winner != None : 
+            return True
 
     def is_tied(self):
-        pass
+        if self.is_finished() and self.winner == None : 
+            return True
 
     def get_winner(self):
-        pass
+        return self.winner
 
     def next_turn(self):
-        pass
-
+        return self.next
+    
     def move(self, player, row, col):
-        pass
+        if(row > 2 or col > 2 or col < 0 or row < 0): raise InvalidMovementException
+        # make sure slot is empty and player is the next player 
+        if(self.board.empty(row , col) and player == self.next and not self.is_finished() ):
+            # make move 
+            self.board.move(player , row , col)
+            print('success')
+            # update player.next
+            self.next = self.p1 if self.next == self.p2 else self.p2
+        else:
+            raise InvalidMovementException
 
 
 BOARD_TEMPLATE = """
@@ -36,20 +78,41 @@ _____|_____|_____
 
 
 class Board(object):
+    #empty = True;
     def __init__(self, initial_board=None):
-        pass
+        temp = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ]
+        self.board = initial_board if (initial_board) else temp 
+    
+    def empty(self , row , col):
+        return self.board[row][col] == None
 
     def move(self, figure, row, col):
-        pass
+        self.board[row][col] = figure
 
     def __str__(self):
-        return BOARD_TEMPLATE  # .format()
+        li = []
+        for i in self.board:
+            for j in i : 
+                li.append('-' if j == None else j)
+                
+        return BOARD_TEMPLATE.format(*li)
 
     def get_row(self, row_number):
-        pass
+        return self.board[row_number]
 
     def get_column(self, col_number):
-        pass
+        col = []
+        for i in self.board:
+            col.append(i[col_number])
+        return col
 
     def get_diagonal(self, row_number):
-        pass
+        if(row_number == 0):
+            return [self.board[0][0] , self.board[1][1] , self.board[2][2]]
+        else:
+            return [self.board[0][2] , self.board[1][1] , self.board[2][0]]
+    
