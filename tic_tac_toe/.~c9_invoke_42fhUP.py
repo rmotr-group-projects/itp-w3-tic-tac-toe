0c0e65ef0b710983214bb6b1ad237ba1be2c5cda
Abayomi <kabayomi007@gmail.com>
2017-03-04 20:00:51 +0000

    Long one!

--- tic_tac_toe/.~c9_invoke_42fhUP.py
+++ tic_tac_toe/.~c9_invoke_42fhUP.py
@@ -1,6 +1,5 @@
-"""
 class Board(object):
-    def __init__(self, initial_board = None):
+    def __init__(self, initiboard = None):
         if initial_board == None:
             self.board = initial_board
         else:
@@ -8,16 +7,18 @@ class Board(object):
             [None, None, None],
             [None, None, None],
             [None, None, None]
-            ]
+        self.board =
+            
+            
     def get_row(self, row_number):
         return self.board[row_number] # Simple
     
     def get_column(self, col_number):
-        return [self.board[0][col_number], self.board[1][col_number], self.board[2][col_number]]
+        return [self.board[0][col_number], self.board[1]col_number, self.board[2]col_number]
     
     def get_diagonal(self, number):
         if number == 0:
             return [self.board[0][0], self.board[1][1], self.board[2][2]]
-        elif number == 1:
+            elif number == 1:
                 return [self.board[0][2], self.board[1][1], self.board[2][0]]
-   """
+                
